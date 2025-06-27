8a207bd4377e8836bac40355d7bfb65472fc5fc0
unknown <stutidamledevops>
2025-06-27 20:22:16 +0530

    up

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-01"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
